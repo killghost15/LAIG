3313b7114b12eac400acb0ea674a4d6f9a079376
caseirouowotm8 <caseirouowotm8@users.noreply.github.com>
2016-10-17 01:40:32 +0100

    Node structure changed.

--- reader/DSXnode.js
+++ reader/DSXnode.js
@@ -2,9 +2,9 @@ function DSXnode(scene){
   this.children=[];
   this.texture;
   this.material;
-  this.drawtype=null;
+  this.primitive;
   this.matrix=mat4.create();
-  this.args=null;
+  this.args=[];
 }
 
 DSXnode.prototype.addChild = function (id) {
@@ -20,7 +20,7 @@ DSXnode.prototype.setMaterial = function (id) {
 };
 
 DSXnode.prototype.setType = function (id) {
-  this.drawtype = id;
+  this.primitive = id;
 };
 
 DSXnode.prototype.setMatrix = function (mat) {
@@ -31,54 +31,32 @@ DSXnode.prototype.setArgs = function (args) {
   this.args = args;
 };
 
-DSXnode.prototype.setActiveMaterial = function (material, m_index) {
-  if((m_index = scene.materialIdList.indexOf(material)) != -1){
-    if((var t_index = scene.textureIdList.indexOf(this.texture)) != -1){
-      scene.materiaList[m_index].setTexture(scene.textureList[t_index]);
+DSXnode.prototype.setActiveMaterial = function (material) {
+  for(var i = 0; i < scene.materialList.length; i += 2){
+    if(this.material == scene.materialList[i]){
+      for(var j = 0; j < scene.texturesList.length; j += 4){
+        if(this.texture == scene.texturesList[j]){
+          scene.materialList[m_index].setTexture(scene.texturesList[j+1]);
+        }
+      }
     }
   }
 };
 
 DSXnode.prototype.display = function (scene, material, M) {
   var trans_matrix = mat4.create();
-  var m_index = null;
   mat4.multiply(trans_matrix, M, this.matrix);
-  if(this.material[0] != "inherit"){
+  if(this.material != "inherit"){
     material = this.material;
   }
-  setActiveMaterial(material, m_index);
+  setActiveMaterial(material);
   if(this.children.length == 0){
-    //Push matrix, set material and texture, etc.
     scene.pushMatrix();
-    if(m_index >= 0){
-      scene.def_app = scene.materialList[m_index];
-    }else{
-      scene.def_app = scene.fallbackMaterial;
-    }
-    scene.def_app.apply();
     scene.multMatrix(this.trans_matrix);
-    switch(this.drawtype){
-      case "triangle":
-      var shape = MyTriangle(scene /*ARGS*/);
-      break;
-      case "rectangle":
-      var shape = MyQuad(scene /*ARGS*/);
-      break;
-      case "cylinder":
-      var shape = MyCylinder(scene /*ARGS*/);
-      break;
-      case "sphere":
-      var shape = MySphere(scene /*ARGS*/);
-      break;
-      case "torus":
-      var shape = MyNightmare(scene /*ARGS*/);
-      break;
-      default:
-      console.log(this.drawtype, "is not a supported shape. Use only one of these: triangle, rectangle, cylinder, sphere, torus");
-      break;
+    //Check primitive, give it a mat and tex then draw
+  }else{
+    for(var i = 0; i < this.children.length; i++;){
+      this.children[i].display(scene, material, M);
     }
-  }
-  for(var i = 0; i < this.children.length; i++;){
-    this.children[i].display(scene, material, M);
   }
 };
